ce21c7f65101d0ae9afa8ee3550c5af2618168b0
rahul gotrekiya <121397381+RahulGotrekiya@users.noreply.github.com>
2025-07-28 23:36:06 +0530

    Add core data structures and global variables
    - Created User structure for account management
    - Created Message structure for chat functionality
    - Added global arrays for storing users and messages
    - Initialized counters and current user tracking
    - Maximum 20 users and 50 messages supported

--- MAIN.C
+++ MAIN.C
@@ -13,10 +13,32 @@
 #include <string.h>
 #include <stdlib.h>
 
+/* Structure Definitions */
+struct User {
+	char username[20];
+	char password[15];
+	char fullname[30];
+};
+
+struct Message {
+	char sender[20];
+	char receiver[20];
+	char text[100];
+	int isRead;
+};
+
+/* Global Variables */
+struct User users[20];
+struct Message message[50];
+int userCount = 0;
+int msgCount = 0;
+char currentUser[20];
+
+
 int main()
 {
 	clrscr();
-	printf("Chat application i c");
+	printf("Chat application in c");
 	getch();
 	return 0;
 }
